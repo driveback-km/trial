3b161357765eab5609ff34fb926d1f4fcc0241d6
kirill.maslov <kirill.maslov@take5people.com>
2018-07-02 16:31:47 +0300

    deploy fixes

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-heroku plugins:install @heroku-cli/plugin-container-registry
 docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
 heroku container:release web --app $HEROKU_APP_NAME
-heroku container:pull web --app $HEROKU_APP_NAME
-heroku open --app $HEROKU_APP_NAME
